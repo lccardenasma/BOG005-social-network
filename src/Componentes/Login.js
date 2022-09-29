ae569e499a48289b60004031c3a768722e0d1420
LauraCardenas <laurac1996@outlook.es>
2022-09-29 11:07:27 -0500

    Primer clase añadida para poner estilo a la página

--- src/Componentes/Login.js
+++ src/Componentes/Login.js
@@ -1,22 +1,22 @@
-import { onNavigate } from '../main.js';
+import { onNavigate } from "../main.js";
 
 export const login = () => {
-  const div = document.createElement('div');
-  const title = document.createElement('h2'); // logo
-  const buttonLogin = document.createElement('button');
-  const buttonBack = document.createElement('button');
-  const inputEmail = document.createElement('input');
-  const inputPass = document.createElement('input');
+  const div = document.createElement("div");
+  const title = document.createElement("h2"); // logo
+  const buttonLogin = document.createElement("button");
+  const buttonBack = document.createElement("button");
+  const inputEmail = document.createElement("input");
+  const inputPass = document.createElement("input");
+  document.body.style.background = "#F3F3F3";
+  buttonLogin.textContent = "Ingresar";
+  buttonBack.textContent = "Atras";
 
-  buttonLogin.textContent = 'Ingresar';
-  buttonBack.textContent = 'Atras';
-
-  buttonLogin.addEventListener('click', () => {
+  buttonLogin.addEventListener("click", () => {
     // console.log(inputEmail.value);
-    onNavigate('/wall');
+    onNavigate("/wall");
   });
-  buttonBack.addEventListener('click', () => {
-    onNavigate('/');
+  buttonBack.addEventListener("click", () => {
+    onNavigate("/");
   });
 
   div.append(title, inputEmail, inputPass, buttonLogin, buttonBack);

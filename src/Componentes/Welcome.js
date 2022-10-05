eb0b4ed73eabacb1ae71a6b09775efc7f75a6ba9
LauraCardenas <laurac1996@outlook.es>
2022-10-05 10:52:47 -0500

    Primer avance en todas las vistas CSS

--- src/Componentes/Welcome.js
+++ src/Componentes/Welcome.js
@@ -1,27 +1,36 @@
-import { onNavigate } from '../main.js';
+import { onNavigate } from "../main.js";
 
 export const welcome = () => {
-  const div = document.createElement('div');
-  const title = document.createElement('h2');
-  const frase = document.createElement('p');
-  const o = document.createElement('p');
-  const buttonLogin = document.createElement('button');
-  const buttonRegister = document.createElement('button');
-  const buttonGoogle = document.createElement('button');
+  const div = document.createElement("div");
+  const title = document.createElement("h2");
+  const frase = document.createElement("p");
+  const o = document.createElement("p");
+  const buttonLogin = document.createElement("button");
+  const buttonRegister = document.createElement("button");
+  const buttonGoogle = document.createElement("button");
 
-  buttonLogin.textContent = 'Iniciar Sesión';
-  buttonRegister.textContent = 'Registrarse';
-  buttonGoogle.textContent = 'Inicia sesión con Google';
-  title.textContent = 'Logo';
-  frase.textContent = 'Una Comunidad para compartir contenido de libros y fragmentos de texto .';
-  o.textContent = 'o';
+  document.body.style.background = "#00008B";
+  div.classList.add("divWelcome");
+  title.classList.add("titleWelcome");
+  frase.classList.add("frase");
+  o.classList.add("o");
+  buttonLogin.classList.add("buttonLoginW");
+  buttonRegister.classList.add("buttonRegisterW");
+  buttonGoogle.classList.add("buttonGoogle");
 
-  buttonLogin.addEventListener('click', () => {
-    onNavigate('/login');
+  buttonLogin.textContent = "INICIAR SESIÓN";
+  buttonRegister.textContent = "REGISTRARSE";
+  buttonGoogle.textContent = "Inicia sesión con Google";
+  title.textContent = "Logo";
+  frase.textContent = "Una Comunidad para compartir contenido de libros y fragmentos de texto .";
+  o.textContent = "o";
+
+  buttonLogin.addEventListener("click", () => {
+    onNavigate("/login");
   });
 
-  buttonRegister.addEventListener('click', () => {
-    onNavigate('/register');
+  buttonRegister.addEventListener("click", () => {
+    onNavigate("/register");
   });
   div.append(title, frase, buttonLogin, buttonRegister, o, buttonGoogle);
 

7add7dc70065dd2db2b9c3fe738ded03e5b6be66
LauraCardenas <laurac1996@outlook.es>
2022-10-11 12:27:57 -0500

    Cambio de nodo a template pag. Register

--- src/Componentes/Register.js
+++ src/Componentes/Register.js
@@ -1,9 +1,16 @@
 import { onNavigate } from "../main.js";
 
-export const register = () => {
-  buttonContinue.addEventListener("click", () => {
-    onNavigate("/wall");
-  });
-};
+export const register = `
+<section class="containerRegister>
+<input type="email" class="email" placeholder="Correo Electrónico">
+<input type="password" class="password" placeholder="Contraseña">
+<button class="botonRegister">Registrarse</button>
+</section>
+`;
 
+/*const buttonRegister = document.getElementsByClassName("registrarse");
+buttonRegister.addEventListener("click", () => {
+  onNavigate("/wall");
+});
+*/
 //Función con Firebase

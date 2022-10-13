78a00086fb6f511b6509f7963f8352d532569c2c
LauraCardenas <laurac1996@outlook.es>
2022-10-13 12:56:16 -0500

    Ingreso con Google

--- src/Index.js
+++ src/Index.js
@@ -1,7 +1,7 @@
 // import { getAuth. onAuthStateChanged } from 'firebase/auth';
 // import { getFirestore, collection, getDocs, getDoc } from 'firebase/firestore';
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.11.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.11.0/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.11.0/firebase-auth.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -21,7 +21,7 @@ console.log("configuración realizada: ", app);
 
 const auth = getAuth(app);
 
-//Formula para registrar usuario con Firebase
+//Función para registrar usuario con Firebase
 
 export const registrar = (email, password) => {
   createUserWithEmailAndPassword(auth, email, password)
@@ -37,7 +37,7 @@ export const registrar = (email, password) => {
     });
 };
 
-//Formula para ingresar a la cuenta con Firebase
+//Función para ingresar a la cuenta con Firebase
 
 export const ingresar = (email, password) => {
   signInWithEmailAndPassword(auth, email, password)
@@ -50,4 +50,43 @@ export const ingresar = (email, password) => {
       const errorMessage = error.message;
       console.log("error:", errorCode, errorMessage);
     });
+};
+
+//Función para ingresar con Google
+
+export const ingresoGoogle = () => {
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+      //console.log("ingreso Google correcto");//
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.customData.email;
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      console.log("error Google:", errorCode, errorMessage, email, credential);
+      // ...
+    });
+  /*getRedirectResult(auth)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+
+      const user = result.user;
+      //console.log("recibidos datos");//
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.customData.email;
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // console.log("error datos recibidos:", errorCode, errorMessage, email, credential);//
+      // ...
+    });*/
 };

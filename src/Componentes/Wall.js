bc79f68a4eb27436b7918e5446070e1a06bb4f96
LauraCardenas <laurac1996@outlook.es>
2022-10-18 16:07:32 -0500

    Avance CSS y Firestore

--- src/Componentes/Wall.js
+++ src/Componentes/Wall.js
@@ -1,9 +1,40 @@
-export const wall = () => {
-  const div = document.createElement("div");
-  const text = document.createElement("p");
-  document.body.style.background = "#AC48D9";
-  text.textContent = "Este es mi muro";
+import { saveTask, getTask } from "../lib/firestore.js";
 
-  div.append(text);
-  return div;
-};
+export function showWall() {
+  const wall = `
+  <section class="containerWall">
+    <h1 class = "inicio"> Inicio </h1>
+    <figure id = "imagenPerfil"> </figure>
+    <form id="taskform" class= "task-form">
+    <textarea type="text" id="postText" class="textPost" placeholder="¿Que quieres compartir?"></textarea>
+    <button class="botonPublicar"> Publicar </button>
+    </form>
+  </section>
+  `;
+  const nodeWall = document.createElement("div");
+  nodeWall.innerHTML = wall;
+  /* const creandoPost = nodeWall.querySelector(".textPost");
+  const botonPublicar = nodeWall.querySelector(".botonPublicar");
+  botonPublicar.addEventListener("click", () => {
+    publicando(creandoPost.value);
+  });*/
+
+  window.addEventListener("DOMContentLoaded", async () => {
+    const querySnapshot = await getTask();
+    console.log(querySnapshot);
+  });
+  const taskForm = nodeWall.querySelector(".task-form");
+
+  taskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("submitted");
+
+    const inputText = taskForm["postText"];
+    console.log(inputText.value);
+
+    saveTask(inputText.value);
+    taskForm.reset();
+  });
+
+  return nodeWall;
+}

03b4a54f315ede317fbb7f0d988400431847af16
LauraCardenas <laurac1996@outlook.es>
2022-10-24 18:12:56 -0500

    eliminar post y avance en CSS

--- src/lib/firestore.js
+++ src/lib/firestore.js
@@ -1,19 +1,23 @@
-import { getFirestore, addDoc, collection, getDocs, onSnapshot, doc, deleteDoc } from 'https://www.gstatic.com/firebasejs/9.11.0/firebase-firestore.js';
-console.log('getDocs: ', getDocs);
+import { getFirestore, addDoc, collection, getDocs, onSnapshot, doc, deleteDoc, getDoc } from "https://www.gstatic.com/firebasejs/9.11.0/firebase-firestore.js";
+console.log("getDocs: ", getDocs);
 const db = getFirestore();
 
 export const saveTask = (text) => {
-  addDoc(collection(db, 'tasks'), { text });
+  addDoc(collection(db, "tasks"), { text });
 };
 
 export const getTask = () => {
-  getDocs(collection(db, 'tasks'));
+  getDocs(collection(db, "tasks"));
 };
 
 export const getAllTasks = (querySnapshot) => {
-  onSnapshot(collection(db, 'tasks'), querySnapshot);
+  onSnapshot(collection(db, "tasks"), querySnapshot);
 };
 
 export const eliminarPost = (id) => {
-  deleteDoc(doc(db, 'tasks', id));
+  deleteDoc(doc(db, "tasks", id));
+};
+
+export const getPost = (id) => {
+  up(doc(db, "tasks", id));
 };
